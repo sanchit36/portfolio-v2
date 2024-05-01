b2958c55aae473968d6fb2c53c72833a9637da8b
Sanchit Bhadgal <121923695+sanchit315@users.noreply.github.com>
2024-05-01 19:21:41 +0530

    fix

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/api/",
+  baseURL: process.env.VERCEL_URL
+    ? `${process.env.VERCEL_URL}/api/`
+    : "http://127.0.0.1:3000/api",
 });
 
 export default instance;
